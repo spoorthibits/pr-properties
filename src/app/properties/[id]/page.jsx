927b76820d1ba11ade88d5244df67a3cb6ae3072
Your Name <your-email@example.com>
2026-06-22 11:19:03 +0530

    updated properties

--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -1,9 +1,9 @@
-import PropertyDetails from '@/components/PropertyDetails';
+// import PropertyDetails from '@/components/PropertyDetails';
 
 
-export default function PropertyDetailsRoute({ params }) {
-    return (<>
-     <PropertyDetails />
-    </>  
-    );
-}
+// export default function PropertyDetailsRoute({ params }) {
+//     return (<>
+//      <PropertyDetails />
+//     </>  
+//     );
+// }
